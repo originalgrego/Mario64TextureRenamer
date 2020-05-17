4b9dafb62b334428683cea773ff2416f3576c778
Grego Lewandowski <original_grego@yahoo.com>
2020-05-17 12:28:15 -0400

    Handles actors/levels folders as well.

--- Program.cs
+++ Program.cs
@@ -9,39 +9,36 @@ namespace Mario64TextureRenamer {
             return number.ToString("X" + hexDigits);
         }
 
+        static void rescursiveDig(String directoryName, List<String> filesList) {
+            IEnumerable<String> data = Directory.EnumerateDirectories(directoryName);
+            foreach (String directory in data) {
+                IEnumerable<String> files = Directory.EnumerateFiles(directory);
+                filesList.AddRange(files);
+
+                rescursiveDig(directory, filesList);
+            }
+        }
+
         static void Main(string[] args) {
             List<String> images = new List<String>();
-            IEnumerable<String> data = Directory.EnumerateDirectories(args[0]);
-            foreach (String directory in data) {
-                Console.WriteLine(directory);
-                IEnumerable<String> files = Directory.EnumerateFiles(directory);
-                images.AddRange(files);
-            }
+            rescursiveDig(args[0], images);
 
             List<String> rawDataFiles = new List<String>();
-            IEnumerable<String> data2 = Directory.EnumerateDirectories(args[1]);
-            foreach (String directory in data2) {
-                Console.WriteLine(directory);
-                IEnumerable<String> files = Directory.EnumerateFiles(directory);
-                rawDataFiles.AddRange(files);
-            }
+            rescursiveDig(args[1], rawDataFiles);
 
-            bool isWindows = Environment.OSVersion.Platform.ToString().ToLower().Contains("win");
             foreach (String imageName in images) {
-                int index;
-                if (isWindows) {
-                    index = imageName.LastIndexOf("\\");
-                } else {
-                    index = imageName.LastIndexOf("/");
-                }
-                String sub = imageName.Substring(index + 1, imageName.Length - 5 - index);
-                foreach (String rawFileName in rawDataFiles) {
-                    if (rawFileName.Contains(sub)) {
-                        byte[] bytes = File.ReadAllBytes(rawFileName);
-                        uint result = XXHash.Hash32(bytes);
-                        Console.WriteLine("Found one - " + sub + " crc " + getHex((int)result, 8) + rawFileName);
-                        byte[] imageData = File.ReadAllBytes(imageName);
-                        File.WriteAllBytes(args[2] + getHex((int)result, 8) + ".png", imageData);
+                if (imageName.Contains(".png")) {
+                    int count = 0;
+                    String sub = imageName.Substring(args[0].Length, imageName.Length - 5 - args[0].Length);
+                    foreach (String rawFileName in rawDataFiles) {
+                        if (!rawFileName.Contains(".c") && rawFileName.Contains(sub)) {
+                            byte[] bytes = File.ReadAllBytes(rawFileName);
+                            uint result = XXHash.Hash32(bytes);
+                            Console.WriteLine("Found one " + count + " - " + sub + " crc " + getHex((int)result, 8) + rawFileName);
+                            byte[] imageData = File.ReadAllBytes(imageName);
+                            File.WriteAllBytes(args[2] + getHex((int)result, 8) + ".png", imageData);
+                            count++;
+                        }
                     }
                 }
             }
